bc40b01bda192470bcd7c7cead9aa70dab88cdc8
Akhilgoud01 <akhilgoud5491@gmail.com>
2024-07-27 14:11:25 +0530

    Initial commit of XCOUNTRIES project

---
+++ xcountries/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CountryList from './CountryList';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>XCOUNTRIES</h1>
+      </header>
+      <CountryList />
+    </div>
+  );
+};
+
+export default App;
